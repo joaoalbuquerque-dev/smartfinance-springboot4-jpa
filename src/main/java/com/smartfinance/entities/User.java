41a37a82f242aa5b58b8cdee1c7a2a5a339bc025
João Albuqerque <devjoao66@gmail.com>
2026-09-07 00:45:27 -0300

    H2 database, test profile, JPA

--- src/main/java/com/smartfinance/entities/User.java
+++ src/main/java/com/smartfinance/entities/User.java
@@ -1,10 +1,15 @@
 package com.smartfinance.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable  {
-
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
