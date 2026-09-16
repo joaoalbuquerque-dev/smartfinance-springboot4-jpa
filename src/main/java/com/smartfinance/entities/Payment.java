60e9848211f78d94f3cf1792d840d529d61b5828
João Albuqerque <devjoao66@gmail.com>
2026-09-16 16:06:25 -0300

    create entity Payment and relationship with Installment

---
+++ src/main/java/com/smartfinance/entities/Payment.java
@@ -0,0 +1,66 @@
+package com.smartfinance.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant paidAt;
+
+    @OneToOne
+    @JoinColumn(name = "installment_id")
+    private Installment installment;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant paidAt, Installment installment) {
+        this.id = id;
+        this.paidAt = paidAt;
+        this.installment = installment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getPaidAt() {
+        return paidAt;
+    }
+
+    public void setPaidAt(Instant paidAt) {
+        this.paidAt = paidAt;
+    }
+
+    public Installment getInstallment() {
+        return installment;
+    }
+
+    public void setInstallment(Installment installment) {
+        this.installment = installment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
