0e75089934ed5683036ec7e99354364b36e08724
João Albuqerque <devjoao66@gmail.com>
2026-09-21 01:53:42 -0300

    feat: add exception handling update

--- src/main/java/com/smartfinance/services/UserService.java
+++ src/main/java/com/smartfinance/services/UserService.java
@@ -4,6 +4,7 @@ import com.smartfinance.entities.User;
 import com.smartfinance.repositories.UserRepository;
 import com.smartfinance.services.exceptions.DataBaseException;
 import com.smartfinance.services.exceptions.ResourceNotFoundExcepetion;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundExcepetion(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
