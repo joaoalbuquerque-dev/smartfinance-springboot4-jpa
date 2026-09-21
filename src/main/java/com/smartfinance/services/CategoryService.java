214bf9f48a485865c76ae4432fc9864f11c6ffc1
João Albuqerque <devjoao66@gmail.com>
2026-09-21 02:35:21 -0300

    feat: Category insert

--- src/main/java/com/smartfinance/services/CategoryService.java
+++ src/main/java/com/smartfinance/services/CategoryService.java
@@ -22,4 +22,8 @@ public class CategoryService {
        Optional<Category> obj = repository.findById(id);
        return obj.get();
     }
+
+    public Category insert(Category obj) {
+        return repository.save(obj);
+    }
 }
